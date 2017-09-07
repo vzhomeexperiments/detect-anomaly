0fc072f0a3d3ffc0391a73591e05f1e413c7982c
Vladimir Zhbanko <vz.home.experiments@gmail.com>
2017-09-07 20:54:21 +0200

    Add files via upload

---
+++ server.R
@@ -0,0 +1,178 @@
+# ----------------------------
+# Description: Shiny app for Udemy course "Identify Problem with Artificial Intelligence"
+# Author: Vladimir Zhbanko
+# Date: 2017-08-27
+# Version: 00.01
+# Date: 
+# Version: 
+# Changed: 
+# ----------------------------
+library(shiny)
+library(tidyverse)
+library(scales)
+library(plyr)
+
+# ================================= 
+# importing data (code will be run once)
+
+# data frame containing information from multiple sensors
+DF_Data <- read_csv("DF_Data.csv")
+# data frame containing equipment information
+DF_Equipm <- read_csv("DF_EquipmData.csv")
+# data frame containing Event Names
+DF_EvCode <- read_csv("DF_EvCodeData.csv")
+
+# Data manipulation and saving to the DF_TEMP
+DF_TEMP <- DF_Data %>% 
+  # arrange by date 
+  arrange(StartDateTime) %>% 
+  # join to decode equipment serial number
+  inner_join(DF_Equipm, by = "IDEquipment") %>% 
+  # select only column needed
+  select(StartDateTime, Name, EventCode, TimeTotal) %>% 
+  # join to decode Event Code meaning
+  inner_join(DF_EvCode, by = "EventCode") %>% 
+  # select only column needed
+  select(StartDateTime, Name, EventCode, TimeTotal, EventText)
+# ================================= 
+
+shinyServer(function(input, output) {
+
+# =================================  
+  # save variables to use in other reactive functions and render functions
+  StartDate <- reactive( {as.POSIXct(input$DateStart)} )
+  EndDate <- reactive( {as.POSIXct(input$DateEnd)} )
+  StatErr <- reactive( {input$cboxSE} )
+  # # uncomment for debugging...
+  # StartDate <- "2017-04-20 00:10:20"
+  # EndDate <- "2017-08-20 00:10:20"
+  # StatErr <- FALSE
+  
+# =================================  
+
+# =================================
+    # save as data frame data used for statistics in other render functions
+  DF_SUM <- reactive({
+    
+    DF_TEMP %>% 
+      # filters for categories
+      filter(EventText == input$selInput) %>% 
+      # group by 
+      group_by(Name) %>% 
+      # filters X date
+      filter(StartDateTime > StartDate(), StartDateTime < EndDate())
+  })
+  
+# =================================  
+  # This object is needed exclusively for clustering
+  DF_SUM_ALL <- reactive({
+    
+    # Data manipulation and saving to the DF_Data reactive value
+    DF_KM <- DF_TEMP %>% 
+      # filters for category
+      filter(EventText == input$Step) 
+    
+    KM <- DF_KM %>% 
+      select(Name, TimeTotal) %>%
+      mutate(Name = revalue(Name, c("Machine #1" = "1", "Machine #2" = "2", "Machine #3" = "3", "Machine #4" = "4"))) %>% 
+      kmeans(centers = 2, nstart = 20)
+    
+      # saving clustering result to the new data frame
+      vector <- as.data.frame.vector(KM$cluster)
+      names(vector) <- "Clust"
+    
+    DF_SUM_ALL <- DF_KM %>% 
+      select(StartDateTime, TimeTotal, Name) %>%
+      # join clustering result
+      bind_cols(vector) %>% 
+      mutate(Clust = as.factor(Clust))  
+      
+  })
+  
+  # # Updating inputs ...
+  # observe({
+  #   x <- input$selInput
+  #   
+  #   # Can also set the label and select items
+  #   updateSelectInput(session, "selInput1",
+  #                     label = "Choose One Step",
+  #                     choices = x,
+  #                     selected = head(x, 1)
+  #   )
+  # })
+  
+  
+# =================================  
+# OUTPUTS
+# =================================  
+    
+  ### Render function to create a main plot:
+  output$Plot <- renderPlot({
+    # generate object for the plot using DF_SUM
+    DF_SUM() %>% 
+      ggplot(aes(x = StartDateTime, y = TimeTotal, col = EventText)) + 
+      geom_smooth(alpha = 0.5, se = StatErr()) +
+      facet_wrap(~Name) + ylab("Duration of Step, seconds") +
+      ggtitle(paste("Overview of CIP/SIP Steps ", "from: ",
+                                      StartDate(), " to: ", EndDate(), sep = "")) 
+  })
+  
+  # ================================= 
+  
+  ### Render function to create a main plot:
+  output$Plot1 <- renderPlot({
+    # generate object for the plot using DF_SUM
+    DF_SUM()  %>% 
+      ggplot(aes(x = StartDateTime, y = TimeTotal, col = EventText)) + geom_point()+
+      geom_smooth(alpha = 0.5, se = StatErr()) +
+      facet_wrap(~Name) + 
+      ylab("Duration of Step, seconds") +
+      ggtitle(paste("Overview of CIP/SIP Steps ", "from: ",
+                                      StartDate(), " to: ", EndDate(), sep = "")) 
+  })
+  
+  # ================================= 
+  
+  ### Render function to create another plot:
+  output$Plot2 <- renderPlot({
+    # generate object for the plot using DF_SUM
+    DF_SUM() %>% 
+      ggplot(aes(x = StartDateTime, y = TimeTotal, col = EventText)) + geom_boxplot() +
+      facet_wrap(Name ~ EventText) +
+      ggtitle(paste("Overview of CIP/SIP Steps ", "from: ",
+                                      StartDate(), " to: ", EndDate(), sep = "")) 
+  })
+  
+  
+  # ================================= 
+  ### Render function to create another plot:
+  output$Plot3 <- renderPlot({
+    # generate object for the plot using DF_SUM
+    DF_SUM_ALL() %>% 
+      filter(StartDateTime > StartDate(), StartDateTime < EndDate()) %>% 
+      ggplot(aes(x = StartDateTime, y = TimeTotal, col =Clust)) + geom_point() + facet_wrap(~Name)+
+      
+      ggtitle(paste("Anomaly Detection of the Step Duration", "from: ",
+                    StartDate(), " to: ", EndDate(), ". Different colour indicates potential anomaly", sep = "")) 
+  })
+  
+  
+  # ### Render function to create a data table:
+  
+  # output$table <- DT::renderDataTable({
+  #   #visualize statistics
+  #   DF_SUM() %>%
+  #     group_by(Name) %>%
+  #     filter(AnalogVal != 0) %>%
+  #     summarise(AverageFlowSLM = round(mean(AnalogVal),digits = 1),
+  #               TargetSLM = 6.5,
+  #               From = min(StartDate),
+  #               To = max(StartDate),
+  #               DurationDays = round((To - From)/86400), # 86400 = seconds in 1 day
+  #               WaterSaveOpportunityMcub = round(1.2*DurationDays* (AverageFlowSLM - 6.5))) #assumed 20 hours work x day
+  # })    
+  
+  
+  
+  
+})
---
+++ ui.R
@@ -0,0 +1,70 @@
+# ----------------------------
+# Description: Shiny app for Udemy course "Identify Problem with Artificial Intelligence"
+# Author: Vladimir Zhbanko
+# Date: 2017-08-27
+# Version: 00.01
+# Date: 
+# Version: 
+# Changed: 
+# ----------------------------
+
+library(shiny)
+library(DT)
+
+# Define steps choices for selectInput function (it is containing steps the user can filter)
+stepsChoices <- c("Step 1 SubStep 1",
+                  "Step 1 SubStep 2",
+                  "Step 1 SubStep 3",
+                  "Step 1 SubStep 4",
+                  "Step 2 SubStep 1",
+                  "Step 2 SubStep 2",
+                  "Step 2 SubStep 3",
+                  "Step 2 SubStep 4",
+                  "Step 2 SubStep 5",
+                  "Step 2 SubStep 6",
+                  "Step 2 SubStep 7")
+
+shinyUI(fluidPage(
+  
+  # Page Row 1 == Information Pane ==
+  # Adding Logo [optional]
+  fluidRow(column(2, img(height = 50, width  = 106, src    = "logo.png")),
+           column(9, 
+                  # Application title
+                  titlePanel("Preparation Steps Duration Overview"))),
+  
+  # Adding a horizontal line
+  hr(),
+  
+  # Page Row 2 == User Inputs ==
+  
+  fluidRow(column(2, dateInput(inputId = "DateStart", label = "Insert Start Date", value = "2017-01-01")),
+           column(2, dateInput(inputId = "DateEnd",   label = "Insert End Date")),
+           column(1, checkboxInput(inputId = "cboxSE", label = "Add Stat Error?", value = FALSE, width = NULL))),
+  fluidRow(column(8, selectInput(inputId = "selInput",label = "Add Machine Steps to Analysis", choices = stepsChoices, 
+                                 selected = stepsChoices[1], multiple = TRUE, 
+                                 selectize = TRUE, width = '100%', size = NULL))),
+  
+  # Adding a horizontal line
+  hr(),
+  
+  # Page Row 3 == Plot or other Outputs ==
+  fluidRow(column(12,  
+                  # Show a plot 
+                  mainPanel(
+                    tabsetPanel(
+                      tabPanel("Plot - Smoothed", plotOutput("Plot")),
+                      tabPanel("Plot - Points", plotOutput("Plot1")),
+                      tabPanel("Plot - Box Plot", plotOutput("Plot2")),
+                      tabPanel("Deviation Auto Detection", "Select machine step and choose the dates of interest",
+                               hr(), 
+                               selectInput(inputId = "Step",label = "ChooseStep", choices = stepsChoices, 
+                                           selected = stepsChoices[1], multiple = FALSE, 
+                                           selectize = TRUE, size = NULL), hr(),
+                               plotOutput("Plot3"))
+                    )
+
+                  )
+  ))
+))
+
