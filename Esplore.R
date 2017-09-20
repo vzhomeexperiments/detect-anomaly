8f485760fc158b76d8e9c4e6b2007a7608ef23d6
vzhomeexperiments <vz.home.experiments@gmail.com>
2017-09-20 22:43:54 +0200

    Creating esplore data script gitignore to share the data

---
+++ Esplore.R
@@ -0,0 +1,19 @@
+# Esplore data ideas
+
+# ============= READ DATA =================
+# Read our big data first ... 9 mln rows...
+DF_Data_Seals_All <- readRDS("DF_Data_Seals_All.data")
+
+# Read our small data second ... 
+DF_Data_Seals_Recent <- readRDS("DF_Data_Seals_Recent.data")
+
+# Read our Ev.Codes...
+DF_EvCode <- readRDS("DF_EvCode.data")
+
+# Read our Equipment
+DF_Equipm <- readRDS("DF_Equipm.data")
+
+# Read our Label
+DF_Data_Label <- readRDS("DF_Data_Label.data")
+
+# ============= END OF READ DATA =================
