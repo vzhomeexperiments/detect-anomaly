06ef9b9c67671e64ece250ff4e3723e887e163cc
vzhomeexperiments <vz.home.experiments@gmail.com>
2017-10-10 21:58:51 +0200

    creating xts objects for feature engineering

--- Esplore.R
+++ Esplore.R
@@ -1,18 +1,19 @@
 # Esplore data ideas
 library(tidyverse)
+library(lubridate)
 # ============= READ DATA =================
 # Read our big data first ... 9 mln rows...
 DF_Data_All <- readRDS("DF_Data_Process.data") #
 
 # Read our small data second ... 
-DF_Data_Recent <- readRDS("DF_Data_Process_Recent.data") #
+DF_Data_Recent <- readRDS("DF_Data_Process_Recent.data") 
 
 DF_Equipm <- read_csv("DF_EquipmData.csv")
 # data frame containing Event Names
 DF_EvCode <- read_csv("DF_EvCodeDataProject.csv")
 
 # Data manipulation and saving to the DF_TEMP
-DF_TEMP <- DF_Data_All %>% 
+DF_TEMP <- DF_Data_Recent %>% 
   # join to decode equipment serial number
   inner_join(DF_Equipm, by = "IDEquipment") %>% 
   # join to decode Event Code meaning
@@ -30,5 +31,12 @@ DF_TEMP <- DF_Data_All %>%
 DF_TEMP %>% 
   filter(EventText == "Cutting Process, phase angle") %>% 
   ggplot(aes(x = StartDate, y = AnalogVal, col = Name)) + geom_point()+facet_grid(~Name)
+
+# =================================================================================
+# use xts package to manipulate time data series object and do feature engineering
+library(xts)
+
+# create xts object (matrix and index)
+DF_RecentXTS <- as.xts(DF_TEMP[, -1], order.by = as.POSIXct(DF_TEMP$StartDate))
 
 
