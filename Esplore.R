2d9b8ca281f81d3caa31eefbc7875dbff9e4c368
vzhomeexperiments <vz.home.experiments@gmail.com>
2017-10-12 20:23:08 +0200

    now with function that transform time-series data creating new features

--- Esplore.R
+++ Esplore.R
@@ -42,7 +42,7 @@ DF_TEMP <- DF_Data_Recent %>%
 # ============= JOIN, Visualize DATA =================
 
 
-# creating human readable data
+# creating human readable data and visualize them
 DF_TEMP %>% 
   filter(EventText == "Cutting Process, phase angle") %>% 
   ggplot(aes(x = StartDate, y = AnalogVal, col = Name)) + geom_point()+facet_grid(~Name)
@@ -86,14 +86,61 @@ head(xts_CUT_Ph)
 # now we can return to the dataframe
 cd <- coredata(xts_CUT_Ph) %>% as.data.frame(row.names = F) 
 cd$StartDate <- index(xts_CUT_Ph)
+
+# view dataset we have now
 head(cd)
 
 
+## Turn this into function
+# maybe it's inefficient way to programming, but the result is ready...
+feature_eng_ts <- function(x, funcToApply = c("mean", "min", "max", "sd")){
+  #function will return new dataframe with new features
+  #x = dataframe with columns to perform feature engineering, it must contain data from one specific category
+  #tscolname = column name containing time series data
+  #avcolname - column name contining data to perform manipulations
+  #funcToApply - vector containing functions to apply data transformations
+  # convert to xts object
+  DF_M1_xts <- as.xts(x[-1], order.by = as.POSIXct(x$StartDate))
+  # aggregate by hour and create new features
+  # convert periodicity from seconds to hours and apply some functions to create new features
+  for(i in 1: length(funcToApply)){
+    # apply functions and merge them  
+    res <- period.apply(DF_M1_xts, endpoints(DF_M1_xts, "hours"), funcToApply[i])
+    names(res) <- paste("AnalogVal_", funcToApply[i], sep = "")
+    # merge new features together
+    if(i == 1){
+      DF_M1_newfeatures <- res
+      } else {
+        DF_M1_newfeatures <- merge(DF_M1_newfeatures, res)
+        }
+  }
+  # return to the dataframe
+  DF_M1_NF <- coredata(DF_M1_newfeatures) %>% as.data.frame(row.names = F)
+  DF_M1_NF$StartDate <- index(DF_M1_newfeatures)
+  # return result
+  return(DF_M1_NF)
+}
 
-# let us select and group specific point in the dataframe
-DF1 <- DF_TEMP %>% 
-  filter(EventText == "Tubing Process, phase angle") %>% 
-  arrange(StartDate) %>% 
-  group_by(Name) %>% 
-  
-  head()
+# example of usage
+# we can apply transformation to each specific machine and each specific sub-process
+DF <- DF_TEMP %>% 
+  filter(EventText == "Cutting Process, phase angle") %>% 
+  filter(Name == "Machine #1") %>% 
+  select(StartDate, AnalogVal)
+
+# keep just categories of the data: machine and event text
+DF_Cat <- DF_TEMP %>% 
+  filter(EventText == "Cutting Process, phase angle") %>% 
+  filter(Name == "Machine #1") %>% select(EventText, Name) %>% unique()
+
+# get new dataset with new features
+DF_FE <- feature_eng_ts(DF)
+# join original categories
+DF_FE$EventText <- DF_Cat[1,1]
+DF_FE$Name <- DF_Cat[1,2]
+
+# review obtained dataset
+head(DF_FE)
+
+# plot some variables
+ggplot(DF_FE, aes(x = StartDate, y = AnalogVal_sd))+geom_point()
