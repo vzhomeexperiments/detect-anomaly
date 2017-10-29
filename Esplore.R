4e429ab7912ab7d4be9ad2d75cbf4445c1e019a5
vladdsm <vladimir.zhbanko@gmail.com>
2017-10-29 13:57:39 +0100

    before implementing in shiny app

--- Esplore.R
+++ Esplore.R
@@ -80,89 +80,37 @@ plot(res$anoms)
 
 # as it seems to be the anomalies are distributed around 2 groups...
 
+# ============= READ DATA =================
+# Read our big data first ... 9 mln rows...
+DF_Data_Recent <- readRDS("DF_Data_Process_Recent.data")
+# data about equipment
+DF_Equipm <- read_csv("DF_EquipmData.csv")
+# data frame containing Event Names
+DF_EvCode <- read_csv("DF_EvCodeDataProject.csv")
+
+# Data manipulation and saving to the DF_TEMP
+DF_TEMP1 <- DF_Data_Recent %>% 
+  # join to decode equipment serial number
+  inner_join(DF_Equipm, by = "IDEquipment") %>% 
+  # join to decode Event Code meaning
+  inner_join(DF_EvCode, by = "EventCode") %>% 
+  # select only column needed
+  select(StartDate, Name, AnalogVal, EventText)
+
+# ============= END OF READ DATA =================
+
+
+# extract data for one machine and one sub-process
+DF_M3_Tub_Res1 <- DF_TEMP1 %>% 
+  filter(EventText == "Tubing Process, resistance Ohm") %>% 
+  filter(Name == "Machine #3") %>% 
+  select(StartDate, AnalogVal) %>% 
+  arrange(StartDate)
+
+dim(DF_M3_Tub_Res1) # 332620 rows
 
 
 
 
 
 
-
-
-# convert periodicity from seconds to hours and apply some functions to create new features
-AnalogVal_mean <- period.apply(xts_M1_Cut_Ph, endpoints(xts_M1_Cut_Ph, "hours"), mean)
-AnalogVal_max <- period.apply(xts_M1_Cut_Ph, endpoints(xts_M1_Cut_Ph, "hours"), max)
-AnalogVal__Min <- period.apply(xts_M1_Cut_Ph, endpoints(xts_M1_Cut_Ph, "hours"), min)
-AnalogVal_sd <- period.apply(xts_M1_Cut_Ph, endpoints(xts_M1_Cut_Ph, "hours"), sd)
-# review obtained object
-head(AnalogVal_mean)
-head(AnalogVal_max)
-head(AnalogVal__Min)
-head(AnalogVal_sd)
-
-# merge these features together
-xts_CUT_Ph <- merge(AnalogVal_mean, AnalogVal_max, AnalogVal__Min, AnalogVal_sd)
-
-# view dataset we have now
-head(xts_CUT_Ph)
-
-# now we can return to the dataframe
-cd <- coredata(xts_CUT_Ph) %>% as.data.frame(row.names = F) 
-cd$StartDate <- index(xts_CUT_Ph)
-
-# view dataset we have now
-head(cd)
-
-
-## Turn this into function
-# maybe it's inefficient way to programming, but the result is ready...
-feature_eng_ts <- function(x, funcToApply = c("mean", "min", "max", "sd")){
-  #function will return new dataframe with new features
-  #x = dataframe with columns to perform feature engineering, it must contain data from one specific category
-  #tscolname = column name containing time series data
-  #avcolname - column name contining data to perform manipulations
-  #funcToApply - vector containing functions to apply data transformations
-  # convert to xts object
-  DF_M1_xts <- as.xts(x[-1], order.by = as.POSIXct(x$StartDate))
-  # aggregate by hour and create new features
-  # convert periodicity from seconds to hours and apply some functions to create new features
-  for(i in 1: length(funcToApply)){
-    # apply functions and merge them  
-    res <- period.apply(DF_M1_xts, endpoints(DF_M1_xts, "hours"), funcToApply[i])
-    names(res) <- paste("AnalogVal_", funcToApply[i], sep = "")
-    # merge new features together
-    if(i == 1){
-      DF_M1_newfeatures <- res
-      } else {
-        DF_M1_newfeatures <- merge(DF_M1_newfeatures, res)
-        }
-  }
-  # return to the dataframe
-  DF_M1_NF <- coredata(DF_M1_newfeatures) %>% as.data.frame(row.names = F)
-  DF_M1_NF$StartDate <- index(DF_M1_newfeatures)
-  # return result
-  return(DF_M1_NF)
-}
-
-# example of usage
-# we can apply transformation to each specific machine and each specific sub-process
-DF <- DF_TEMP %>% 
-  filter(EventText == "Cutting Process, phase angle") %>% 
-  filter(Name == "Machine #1") %>% 
-  select(StartDate, AnalogVal)
-
-# keep just categories of the data: machine and event text
-DF_Cat <- DF_TEMP %>% 
-  filter(EventText == "Cutting Process, phase angle") %>% 
-  filter(Name == "Machine #1") %>% select(EventText, Name) %>% unique()
-
-# get new dataset with new features
-DF_FE <- feature_eng_ts(DF)
-# join original categories
-DF_FE$EventText <- DF_Cat[1,1]
-DF_FE$Name <- DF_Cat[1,2]
-
-# review obtained dataset
-head(DF_FE)
-
-# plot some variables
-ggplot(DF_FE, aes(x = StartDate, y = AnalogVal_sd))+geom_point()
