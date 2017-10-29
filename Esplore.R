a865b4263e2ea55c4a3bf00edb802d4645ee36cb
vladdsm <vladimir.zhbanko@gmail.com>
2017-10-29 12:25:56 +0100

    more on AnomalyDetection

--- Esplore.R
+++ Esplore.R
@@ -4,16 +4,13 @@
 #
 library(tidyverse)
 library(xts)
-devtools::install_github("twitter/AnomalyDetection")
+# devtools::install_github("twitter/AnomalyDetection")
 library(AnomalyDetection)
 
 # ============= READ DATA =================
 # Read our big data first ... 9 mln rows...
-DF_Data_All <- readRDS("DF_Data_Process.data") #
-
-# Read our small data second ... 
-DF_Data_Recent <- readRDS("DF_Data_Process_Recent.data") 
-
+DF_Data_All <- readRDS("DF_Data_Process.data")
+# data about equipment
 DF_Equipm <- read_csv("DF_EquipmData.csv")
 # data frame containing Event Names
 DF_EvCode <- read_csv("DF_EvCodeDataProject.csv")
@@ -50,23 +47,23 @@ DF_TEMP %>%
   ggplot(aes(x = StartDate, y = AnalogVal, col = Name)) + geom_point()+facet_grid(~Name)
 
 # =================================================================================
-# use xts package to convert to time data series object
 
-# we can apply transformation to each specific machine and each specific sub-process
+
+# extract data for one machine and one sub-process
 DF_M1_Cut_Ph <- DF_TEMP %>% 
   filter(EventText == "Cutting Process, phase angle") %>% 
   filter(Name == "Machine #3") %>% 
   select(StartDate, AnalogVal)
 
-# create xts object (matrix and index)
-xts_M1_Cut_Ph <- as.xts(DF_M1_Cut_Ph[, -1], order.by = as.POSIXct(DF_M1_Cut_Ph$StartDate))
-# use plot.xts
-plot.xts(xts_M1_Cut_Ph)
-# getting to know the perioficity of the data and the time span
-periodicity(xts_M1_Cut_Ph)
-# getting to know number of hours, seconds, etc
-nseconds(xts_M1_Cut_Ph)
-nhours(xts_M1_Cut_Ph)
+# # create xts object (matrix and index)
+# xts_M1_Cut_Ph <- as.xts(DF_M1_Cut_Ph[, -1], order.by = as.POSIXct(DF_M1_Cut_Ph$StartDate))
+# # use plot.xts
+# plot.xts(xts_M1_Cut_Ph)
+# # getting to know the perioficity of the data and the time span
+# periodicity(xts_M1_Cut_Ph)
+# # getting to know number of hours, seconds, etc
+# nseconds(xts_M1_Cut_Ph)
+# nhours(xts_M1_Cut_Ph)
 
 # example featured by the package Anomaly Detection
 data(raw_data)
@@ -75,9 +72,21 @@ res$plot
 
 # applied to our data
 res = AnomalyDetectionTs(DF_M1_Cut_Ph, max_anoms=0.02, direction='both', plot=TRUE)
+# getting the plot out ot it
 res$plot
 
+# we can also just plot anomalies we have
 plot(res$anoms)
+
+# as it seems to be the anomalies are distributed around 2 groups...
+
+
+
+
+
+
+
+
 
 # convert periodicity from seconds to hours and apply some functions to create new features
 AnalogVal_mean <- period.apply(xts_M1_Cut_Ph, endpoints(xts_M1_Cut_Ph, "hours"), mean)
