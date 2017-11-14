edfd2a444111b162bc5516357b57bccba87ea8e2
vzhomeexperiments <vz.home.experiments@gmail.com>
2017-11-14 18:29:37 +0100

    preparing functions for Shiny App

--- global.R
+++ global.R
@@ -1,5 +1,73 @@
 library(xts)
 library(tidyverse)
+
+
+# function that perform scoring using Deep Learning model to each machine and return common dataframe
+anomalyscore_machines <- function(x, Machines){
+  # apply Neural Network Model for each machine
+  # x - dataframe containing only one selected event for each machine
+  # path to model - location of the Model stored as a .bin object
+  # Machines - vector containing machine names
+  
+  for(i in 1:length(Machines, path_to_model)){
+    
+    DF_A <- x %>% 
+      filter(Name == Machines[i]) %>% 
+      select(StartDate, AnalogVal)
+    DF_B <- x %>% 
+      filter(Name == Machines[i]) %>% 
+      select(EventText, Name) %>% unique()
+    
+    DF_F <- anomalyscore_nn(DF_A, path_to_model)
+    DF_F$EventText <- DF_B[1,1]
+    DF_F$Name <- DF_B[1,2]
+    
+    if(i == 1){
+      DF_SUM <- DF_F
+    } else {
+      DF_SUM <- DF_SUM %>% bind_rows(DF_F)
+    }  
+    
+  }
+  return(DF_SUM)
+}
+
+## Turn this into function
+# function that perform Neural Network on data from every machine and event
+anomalyscore_nn <- function(x, path_to_model){
+  #function will convert data frame to image, apply NN and apply score to the original dataframe
+  #x = dataframe with columns to perform Anomaly Detection scoring, it must contain data from one specific category, machine
+  #path_to_model - path to model
+  
+  
+  
+  
+  # convert to xts object
+  DF_M1_xts <- as.xts(x[ ,-1], order.by = as.POSIXct(x$StartDate))
+  # aggregate by hour and create new features
+  # convert periodicity from seconds to hours and apply some functions to create new features
+  for(i in 1: length(funcToApply)){
+    # apply functions and merge them  
+    res <- period.apply(DF_M1_xts, endpoints(DF_M1_xts, "hours"), funcToApply[i])
+    names(res) <- paste("AnalogVal_", funcToApply[i], sep = "")
+    # merge new features together
+    if(i == 1){
+      DF_M1_newfeatures <- res
+    } else {
+      DF_M1_newfeatures <- merge(DF_M1_newfeatures, res)
+    }
+  }
+  # return to the dataframe
+  DF_M1_NF <- coredata(DF_M1_newfeatures) %>% as.data.frame(row.names = F)
+  DF_M1_NF$StartDate <- index(DF_M1_newfeatures)
+  # removing rows with missing data!!!
+  DF_M1_NF <- na.omit(DF_M1_NF)
+  # return result
+  return(DF_M1_NF)
+}
+
+
+
 ## Turn this into function
 # function to create new features from original feature...
 feature_eng_ts <- function(x, funcToApply = c("mean", "min", "max", "sd")){
